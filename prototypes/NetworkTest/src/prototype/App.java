9da54ae888d863cffc6c885d41bbb5aa0a92b255
Christopher D Canfield <cdcanfield@gmail.com>
2014-01-30 07:16:01 -0500

    Added enough to App so that it displays
    It does not do anything else yet, though.

--- prototypes/NetworkTest/src/prototype/App.java
+++ prototypes/NetworkTest/src/prototype/App.java
@@ -1,11 +1,44 @@
 package prototype;
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * 
  * @author Christopher D. Canfield
  */
-public class App extends JFrame
+public class App extends JPanel
 {
+	private JFrame frame;
+	
+	public static void main(String[] args)
+	{
+		App app = new App();
+		app.start();
+	}
+	
+	public App()
+	{
+		frame = new JFrame("Network Prototype");
+		frame.setSize(600, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(Color.GRAY);
+		frame.setLocationRelativeTo(null);
+		frame.add(this);
+		frame.setVisible(true);
+	}
+	
+	public void start()
+	{
+		//frame.addKeyListener(controller);
+		
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g)
+	{
 
+	}
 }
