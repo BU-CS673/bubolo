ad365245a52624f2aa2a9bd023a105d2ab05c30b
Christopher D. Canfield <cdcanfield@gmail.com>
2014-02-09 17:29:17 -0500

    Added libgdx libraries and Updated graphics system
    Continued implementation of abstract base class Sprite, as well as the
    concrete TankSprite class.

--- bubolo/src/bubolo/graphics/Sprite.java
+++ bubolo/src/bubolo/graphics/Sprite.java
@@ -1,5 +1,7 @@
 package bubolo.graphics;
 
+import java.util.UUID;
+
 import bubolo.world.Entity;
 
 /**
@@ -7,13 +9,49 @@ import bubolo.world.Entity;
  * x,y location.
  * @author BU673 - Clone Industries
  */
-public interface Sprite<T extends Entity>
+public abstract class Sprite<T extends Entity>
 {
+	// Each concrete sprite class has a unique ID. These IDs are shared across
+	// each instance of the concrete sprite.
+	private UUID id;
+	
+	/**
+	 * Constructor for the base Sprite class.
+	 * @param spriteId An id that identifies the sprite's type.
+	 */
+	public Sprite(UUID spriteId)
+	{
+		this.id = spriteId;
+		
+	}
+	
+	/**
+	 * Returns an id that identifies the sprite's type. This exists
+	 * so that the world system can delegate the entity.getSpriteId() method
+	 * to this method.
+	 * @return A UUID that identifies the sprite's type
+	 */
+	public UUID getId()
+	{
+		return id;
+	}
+	
 	/**
 	 * Draws the sprite to the screen.
 	 * @param layer Specifies the draw order.
 	 * @param entity The entity that this sprite represents. At a minimum,
 	 * this is used to get location information.
 	 */
-	public void draw(DrawLayer layer, T entity);
+	public abstract void draw(DrawLayer layer, T entity);
+	
+	/**
+	 * Static factory method for creating concrete sprites. This ensures that
+	 * the world system does not need to know about any of the concrete sprites.
+	 * @param entity Reference to the entity that is creating the sprite.
+	 * @return A new concrete Sprite of type T.
+	 */
+	public static <T extends Entity> T create(T entity)
+	{
+		return null;
+	}
 }
--- bubolo/src/bubolo/graphics/TankSprite.java
+++ bubolo/src/bubolo/graphics/TankSprite.java
@@ -1,13 +1,21 @@
 package bubolo.graphics;
 
+import java.util.UUID;
+
 import bubolo.world.Tank;
 
 /**
  * The graphical representation of a Tank.
  * @author BU673 - Clone Industries
  */
-public class TankSprite implements Sprite<Tank>
+public class TankSprite extends Sprite<Tank>
 {
+	private Texture image;
+	
+	TankSprite()
+	{
+		super(UUID.fromString("13eb9d6a-8965-43fc-a4aa-82fb70c9045f");
+	}
 
 	@Override
 	public void draw(DrawLayer layer, Tank entity)
