c9997bc66dfef909389c1f3bf4e73edd26f62553
ethanms <ethanms@bu.edu>
2014-02-15 16:09:11 -0500

    Adding Textures and Road Sprite
    Added bullet.png
    Added wall.png
    Added RoadSprite.java

---
+++ bubolo/src/bubolo/graphics/RoadSprite.java
@@ -0,0 +1,52 @@
+package bubolo.graphics;
+
+import java.util.UUID;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import bubolo.world.Road;
+
+/**
+ * The graphical representation of a Road
+ * @author BU673 - Clone Industries
+ */
+class RoadSprite extends Sprite<Road>
+{
+	private Texture image;
+	
+	// true if the camera controller has been added.
+	private boolean addedCameraController;
+	
+	/**
+	 * Constructor for the RoadSprite. This is Package-private because sprites
+	 * should not be directly created outside of the graphics system
+	 * (instead, call the Sprite.create(entity) static method).
+	 * @param road Reference to the road that this RoadSprite represents.
+	 */
+	RoadSprite(Road road)
+	{
+		// EMS WHAT IS THIS BELOW?
+		super(UUID.fromString("13eb9d6a-8965-43fc-a4aa-82fb70c9045f"),
+				DrawLayer.TERRAIN_MODIFIERS, road);
+		
+		image = Graphics.getTexture(Graphics.TEXTURE_PATH + "road.png");
+	}
+
+	// EMS Commented out because I don't know to handle this--do we need a draw method for road?
+	// ...or is the layer itself drawn?
+	@Override
+	public void draw(SpriteBatch batch, Camera camera, DrawLayer layer)
+	{
+//		drawTexture(batch, camera, layer, image);
+//		
+//		if (!addedCameraController)
+//		{
+//			// TODO: the sprite needs to ask the Road if it is the local player.
+//			//	Something like road.isLocalPlayer() would work.
+//			CameraController controller = new RoadCameraController(getEntity());
+//			Graphics.getInstance().addCameraController(controller);
+//			addedCameraController = true;
+//		}
+	}
+}
