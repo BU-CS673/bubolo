400fa2be3102d45528f713cbbf4b3739e2857ee7
Christopher D Canfield <cdcanfield@gmail.com>
2014-02-21 18:52:22 -0500

    Set up a large number of Network tests
    - Most need to be implemented, however

---
+++ bubolo/test/bubolo/net/NetworkExceptionTest.java
@@ -0,0 +1,22 @@
+package bubolo.net;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NetworkExceptionTest
+{
+
+	@Test
+	public void testNetworkExceptionString()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNetworkExceptionThrowable()
+	{
+		fail("Not yet implemented");
+	}
+
+}
