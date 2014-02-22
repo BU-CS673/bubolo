ead53f73fa06ae4774f6847e68d8447aa1ac852f
Aaron W. Heuckroth <a.heuckroth@gmail.com>
2014-02-22 14:48:18 -0500

    Recreated parser method in BuboloApplication
    Avoided merge conflicts. Much fixing to do!

--- bubolo/src/bubolo/BuboloApplication.java
+++ bubolo/src/bubolo/BuboloApplication.java
@@ -43,6 +43,77 @@ public class BuboloApplication implements GameApplication
 		this.windowHeight = windowHeight;
 	}
 	
+	/**
+	 * Called to import a map from a tiled JSON file
+	 */
+	private void parser()
+	{
+		int mapHeight = 0;
+		int mapWidth = 0;
+		Object obj = null;
+		JSONObject layerObject = null;
+		JSONArray tileData = null;
+		JSONArray layerArray = null;
+		
+		// TODO: we need a way to determine the size of the game map. Perhaps we can have a default constructor,
+		// and then the map loader or creator could set the size.
+
+		/*
+		 *  TODO: Modify the path line to choose from an assortment of maps either randomly or user selected iff we add more maps. 
+		 */
+		
+		Path path = FileSystems.getDefault().getPath("res/maps", "Map 1.json");
+		Charset charset = Charset.forName("US-ASCII");
+		BufferedReader reader = null;		
+		
+		try
+		{
+			reader = Files.newBufferedReader(path, charset);
+			JSONParser parser = new JSONParser();
+			obj = parser.parse(reader);
+			reader.close();
+			JSONObject jsonObject = (JSONObject) obj;		
+			mapHeight = (int) ((long) jsonObject.get("height"));
+			mapWidth = (int) ((long) jsonObject.get("width"));
+			layerArray = (JSONArray) jsonObject.get("layers");
+			layerObject = (JSONObject) layerArray.get(0);
+			tileData = (JSONArray) layerObject.get("data");
+			
+			world = new GameWorld(mapHeight, mapWidth);
+			
+			for(int i = 0; i < mapHeight; i++)
+			{
+				for(int j = 0; j < mapWidth; j++)
+				{
+					/*
+					 * TODO: Add cases to interpret all of the tileset ID's from tiled.
+					 * Convert string outputs to generate "tiles" within the map
+					 */
+					
+					switch (tileData.get(i * mapWidth + j).toString())
+					{
+						case "1":
+							world.addEntity(new Grass());
+							break;
+							
+						default:
+							break;
+					}
+				}
+			}
+		}
+		catch (IOException e1)
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		catch (ParseException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
 	@Override
 	public boolean isReady()
 	{
