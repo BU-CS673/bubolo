092054e00fe8bbf746d84656189003d4a9bc8df0
Christopher D. Canfield <cdcanfield@gmail.com>
2014-03-17 18:09:15 -0400

    Added TankMovedNetCommand

--- bubolo/src/bubolo/net/command/TankMovedNetCommand.java
+++ bubolo/src/bubolo/net/command/TankMovedNetCommand.java
@@ -4,18 +4,18 @@ import bubolo.net.NetworkCommand;
 import bubolo.world.World;
 
 /**
- * Instructs connected systems to add a new bullet.
+ * Notifies network players that a tank has moved.
  * @author BU673 - Clone Industries
  */
-public class AddBulletNetCommand implements NetworkCommand
+public class TankMovedNetCommand implements NetworkCommand
 {
-	private static final long serialVersionUID = -2484196357990278061L;
-
 	// TODO: add attributes.
 	
 	@Override
 	public void execute(World world)
 	{
-		// TODO Implement this.
+		// TODO Auto-generated method stub
+		
 	}
+
 }
