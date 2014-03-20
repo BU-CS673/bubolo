cfedaf627d777274cb6d674490b82d4483cd7723
Aaron W. Heuckroth <a.heuckroth@gmail.com>
2014-03-20 13:07:16 -0400

    Fixed formatting error in RoadSprite
    Code standards and curly braces will be the death of me.

--- bubolo/src/bubolo/graphics/RoadSprite.java
+++ bubolo/src/bubolo/graphics/RoadSprite.java
@@ -1,6 +1,5 @@
 package bubolo.graphics;
 
-
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -10,22 +9,26 @@ import bubolo.world.entity.concrete.Road;
 
 /**
  * The graphical representation of a Road
+ * 
  * @author BU673 - Clone Industries
  */
 class RoadSprite extends Sprite<Road>
 {
 	private TextureRegion[] frames;
-	
+
 	/**
-	 * Constructor for the RoadSprite. This is Package-private because sprites
-	 * should not be directly created outside of the graphics system.
-	 * @param road Reference to the road that this RoadSprite represents.
+	 * Constructor for the RoadSprite. This is Package-private because sprites should not
+	 * be directly created outside of the graphics system.
+	 * 
+	 * @param road
+	 *            Reference to the road that this RoadSprite represents.
 	 */
 	RoadSprite(Road road)
 	{
 		super(DrawLayer.TERRAIN, road);
-		
-		frames = TextureUtil.adaptiveSplit_16(Graphics.getTexture(Graphics.TEXTURE_PATH + "road.png"));
+
+		frames = TextureUtil.adaptiveSplit_16(Graphics.getTexture(Graphics.TEXTURE_PATH
+				+ "road.png"));
 	}
 
 	@Override
@@ -34,7 +37,7 @@ class RoadSprite extends Sprite<Road>
 		if (isEntityDisposed())
 		{
 			Sprites.getInstance().removeSprite(this);
-			}
+		}
 		else
 		{
 			drawTexture(batch, camera, layer, frames[this.getEntity().getState()]);
