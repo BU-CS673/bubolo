5744541b6d71dd0dd450ff0fb4aec5d866886a8c
Christopher D Canfield <cdcanfield@gmail.com>
2014-03-21 00:53:47 -0400

    Added BulletController

---
+++ bubolo/src/bubolo/controllers/ai/BulletController.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2014 BU MET CS673 Game Engineering Team
+ *
+ * See the file license.txt for copying permission.
+ */
+
+package bubolo.controllers.ai;
+
+import bubolo.controllers.Controller;
+import bubolo.world.World;
+
+/**
+ * @author BU CS673 - Clone Productions
+ */
+public class BulletController implements Controller
+{
+
+	/* (non-Javadoc)
+	 * @see bubolo.controllers.Controller#update(bubolo.world.World)
+	 */
+	@Override
+	public void update(World world)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
