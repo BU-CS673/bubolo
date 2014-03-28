a054cdfffb39eed06365bfbe04d2e1dfc89f9348
pmdouglas <pmdouglas@gmail.com>
2014-03-28 09:55:05 -0400

    standards
    non standard if statement fixed

--- bubolo/src/bubolo/world/GameWorld.java
+++ bubolo/src/bubolo/world/GameWorld.java
@@ -123,7 +123,8 @@ public class GameWorld implements World
         
         Sprites.getInstance().createSprite(entity);
         Controllers.getInstance().createController(entity, controllerFactory);
-        if(entity.getClass() == Tank.class){
+        if(entity.getClass() == Tank.class)
+        {
         	tanks.add(entity);
         }
         entitiesToAdd.add(entity);
