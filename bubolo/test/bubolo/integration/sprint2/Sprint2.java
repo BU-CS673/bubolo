d00adafa3960c75f7ec603b63792cf58c1c57e34
Aaron Heuckroth <a.heuckroth@gmail.com>
2014-04-02 10:53:15 -0400

    Added IP Popup Window to Sprint 2 Demo
    * Allows Server IP Entry
    * Allows re-try of IP entry, if unable to join server (throws an error,
    which is ugly, but functional
    * Correctly closes IP window if another option is selected

--- bubolo/test/bubolo/integration/sprint2/Sprint2.java
+++ bubolo/test/bubolo/integration/sprint2/Sprint2.java
@@ -1,10 +1,14 @@
 package bubolo.integration.sprint2;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import bubolo.integration.CollisionTestApplication;
 import bubolo.integration.NetClientTestApplication;
@@ -13,6 +17,13 @@ import bubolo.net.Network;
 import bubolo.net.NetworkSystem;
 import bubolo.ui.AltMenuScreen;
 import bubolo.ui.LoadingScreen;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SpringLayout;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -26,6 +37,7 @@ public class Sprint2
 {
 	static LoadingScreen s = new LoadingScreen();
 	static AltMenuScreen fp;
+	static JFrame IP_popup;
 
 	/**
 	 * 
@@ -101,8 +113,11 @@ public class Sprint2
 	public static void setupSinglePlayer()
 	{
 		fp.setVisible(false);
+		if (IP_popup != null){
+			IP_popup.setVisible(false);
+		}
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "BUBOLO Tank Controller Integration";
+		cfg.title = "BUBOLO v0.2.0";
 		cfg.width = 1067;
 		cfg.height = 600;
 		cfg.useGL20 = true;
@@ -112,8 +127,11 @@ public class Sprint2
 	public static void setupHostMultiPlayer()
 	{
 		fp.setVisible(false);
+		if (IP_popup != null){
+			IP_popup.setVisible(false);
+		}
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "BUBOLO Net Server Integration";
+		cfg.title = "BUBOLO v0.2.0, Server Client";
 		cfg.width = 1067;
 		cfg.height = 600;
 		cfg.useGL20 = true;
@@ -122,21 +140,52 @@ public class Sprint2
 
 	public static void setupJoinMultiPlayer() throws IOException
 	{
-		fp.setVisible(false);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Server IP Address: ");
-		String addressString;
-		addressString = br.readLine();
-		InetAddress address = Inet4Address.getByName(addressString);
+		//Set IP Address dialog
+		boolean gotIP = false;
+		final JPanel IP_panel = new JPanel(new BorderLayout());
+		final JTextField textField = new JTextField("192.168.1.1");
+		IP_panel.add(textField);
+		JButton joinButton = new JButton("Join Game");
+		IP_panel.add(joinButton, BorderLayout.SOUTH);
+		
+		IP_popup = new JFrame("Enter Server IP Address");
 
-		Network net = NetworkSystem.getInstance();
-		net.connect(address);
+		IP_popup.setLocationRelativeTo(fp);
+		IP_popup.setSize(250, 100);
+		IP_popup.setContentPane(IP_panel);
+	
+		IP_popup.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		IP_panel.setVisible(true);
+		IP_popup.setVisible(true);
 
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "BUBOLO Net Client Integration";
-		cfg.width = 1067;
-		cfg.height = 600;
-		cfg.useGL20 = true;
-		new LwjglApplication(new Sprint2_JoinMultiPlayerApp(1067, 600, address), cfg);
+		joinButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)
+			{
+				String addressString;
+				addressString = textField.getText();
+
+				InetAddress address = null;
+				try
+				{
+					address = Inet4Address.getByName(addressString);
+					Network net = NetworkSystem.getInstance();
+					net.connect(address);
+					LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+					cfg.title = "BUBOLO v0.2.0, Multiplayer Client";
+					cfg.width = 1067;
+					cfg.height = 600;
+					cfg.useGL20 = true;
+					new LwjglApplication(new Sprint2_JoinMultiPlayerApp(1067, 600, address), cfg);
+					IP_popup.setVisible(false);
+					fp.setVisible(false);
+				}
+				catch (UnknownHostException e1)
+				{
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
 	}
 }
