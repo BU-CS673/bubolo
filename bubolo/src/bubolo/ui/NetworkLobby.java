a5e2a7d76caec9d4ea4580cc96e3957b82b90952
Christopher D. Canfield <cdcanfield@gmail.com>
2014-04-09 21:29:50 -0400

    Updates to NetworkLobby

--- bubolo/src/bubolo/ui/NetworkLobby.java
+++ bubolo/src/bubolo/ui/NetworkLobby.java
@@ -4,6 +4,7 @@
 
 package bubolo.ui;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import bubolo.net.NetworkObserver;
@@ -11,54 +12,60 @@ import bubolo.net.NetworkObserver;
 /**
  * @author BU CS673 - Clone Productions
  */
-public class NetworkLobby implements NetworkObserver, GameScreen
+public class NetworkLobby implements NetworkObserver, UiScreen
 {
-	private Stage stage;
-	
+	private Stage screen;
+
 	public NetworkLobby()
 	{
+		screen = new Stage();
+		Gdx.input.setInputProcessor(screen);
+		
 		
 	}
-	
-	
+
 	@Override
 	public void draw()
 	{
-		
+		screen.act(Gdx.graphics.getDeltaTime());
+		screen.draw();
 	}
-	
+
 	@Override
 	public void onConnect(String clientName, String serverName)
 	{
-		
+
 	}
 
 	@Override
 	public void onClientConnected(String clientName)
 	{
-		
+
 	}
 
 	@Override
 	public void onClientDisconnected(String clientName)
 	{
-		
+
 	}
 
 	@Override
 	public void onGameStart(int timeUntilStart)
 	{
-		
+
 	}
-	
+
 	@Override
 	public void onMessageReceived(String message)
 	{
-		
+
 	}
-	
+
+	/**
+	 * Disposes the NetworkLobby. This must be called when the lobby screen is removed.
+	 */
 	public void dispose()
 	{
-		
+		screen.dispose();
 	}
 }
