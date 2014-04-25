c5e1b8fd3605a4dc39f3e20b15e40ab80019f46f
Christopher D. Canfield <cdcanfield@gmail.com>
2014-04-24 20:12:06 -0400

    Warning & typo fixes

--- bubolo/src/bubolo/ui/LobbyScreen.java
+++ bubolo/src/bubolo/ui/LobbyScreen.java
@@ -16,13 +16,12 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 /**
- * The network game lobby, which allows users to message each other before starting the game..
+ * The network game lobby, which allows users to message each other before starting the game.
  * 
  * @author BU CS673 - Clone Productions
  */
