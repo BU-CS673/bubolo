20e51db384d29b7f0e44b703f346523cdebaa704
Christopher D Canfield <cdcanfield@gmail.com>
2014-04-30 12:43:32 -0400

    Updated Main.java launcher

--- bubolo/src/bubolo/Main.java
+++ bubolo/src/bubolo/Main.java
@@ -1,5 +1,8 @@
 package bubolo;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 
 /**
  * The application's entry point.
@@ -7,12 +10,36 @@ package bubolo;
  */
 public class Main
 {
+	private static LwjglApplication application;
+	
 	/**
 	 * The application's entry point. 
 	 * @param args Unused. Arguments passed on startup will be ignored. 
 	 */
 	public static void main(String[] args)
 	{
-		// do nothing...
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "BUBOLO Audio Integration";
+		cfg.width = 600;
+		cfg.height = 600;
+		setApplication(new LwjglApplication(new BuboloApplication(1067, 600), cfg));
+	}
+	
+	/**
+	 * Sets the game application.
+	 * @param application the game application.
+	 */
+	static void setApplication(LwjglApplication application)
+	{
+		Main.application = application;
+	}
+	
+	/**
+	 * Returns a reference to the application.
+	 * @return the game application.
+	 */
+	static LwjglApplication getApplication()
+	{
+		return Main.application;
 	}
 }
