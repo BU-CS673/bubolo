0a5f03c17600f191820705b34d09cd8c6bc8a8b1
ChristopherCanfield <cdcanfield@gmail.com>
2021-02-24 09:58:19 -0500

    Added a test for the map importer

--- bubolo/test/bubolo/map/MapImporterTest.java
+++ bubolo/test/bubolo/map/MapImporterTest.java
@@ -2,13 +2,24 @@ package bubolo.map;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
 import org.junit.Test;
 
 public class MapImporterTest {
 
 	@Test
-	public void importEverardIsland() {
-		fail("Not yet implemented");
+	public void importEverardIsland() throws IOException {
+		MapImporter importer = new MapImporter();
+		MapImporter.Results results = importer.importJsonMap(FileSystems.getDefault().getPath("res", "maps/Everard Island.json"));
+		
+		assertNotNull(results.world());
+		assertEquals(3, results.layerCount());
+		assertEquals(2, results.tilesetCount());
+		assertEquals(11, results.typesImported().size());
+		assertEquals(114, results.tileWidth());
+		assertEquals(64, results.tileHeight());
 	}
 
 }
